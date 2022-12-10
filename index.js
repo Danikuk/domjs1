a68e66b9a31381699297e7f7b8ad24f2b33e707c
Danikuk <soloquedate@gmail.com>
2022-12-11 00:02:08 +0100

    domaci1

---
+++ index.js
@@ -0,0 +1,15 @@
+const uiObjekti = {
+    message: document.getElementById('message'),
+    provjeriKolikoImaSlovaElement: document.getElementById("provjerenoKolikoImaSlova"),
+};
+
+function provjeriKolikoImaSlova(event) {
+    //provjeriti  ima li 10 slova u poruci
+    event.preventDefault();
+  
+    if (uiObjekti.message.value.length > 10 ) {
+      uiObjekti.provjeriKolikoImaSlovaElement.innerHTML = `<p>Poruka ima više od 10 slova!</p>`;
+    } else {
+      uiObjekti.provjeriKolikoImaSlovaElement.innerHTML = `<p>Poruka nema više od 10 slova!</p>`;
+    }
+}
